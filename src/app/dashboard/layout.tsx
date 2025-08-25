a9bffcd565b0b53943202c72508875f98701dd37
esmail <you@example.com>
2025-08-25 14:28:06 +0330

    Component division

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,10 +2,15 @@ import Link from 'next/link'
 import React, { FC, ReactNode } from 'react'
 interface Ichildren {
   children: ReactNode;
+  news: ReactNode;
 }
 
-const Dashboardlayout: FC<Ichildren> = ({ children }) => {
+const Dashboardlayout: FC<Ichildren> = ({ children, news }) => {
   return (
+    <>
+    <div className='flex'>
+        <h1>{news}</h1>
+    </div>
       <div className='flex'>
         <aside className='w-52 bg-white '>
           <h1 className='text-black'>mune Dashboard</h1>
@@ -29,6 +34,7 @@ const Dashboardlayout: FC<Ichildren> = ({ children }) => {
         </aside>
         <main className='grow'>{children}</main>
       </div>
+      </>
   )
 }
 
